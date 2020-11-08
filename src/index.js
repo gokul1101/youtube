1ec73fd6e0bdabd1af2bfc28dcf25311e9b667d2
Gokul S <gokuls2781@gmail.com>
2020-11-08 21:32:28 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom" 
 import App from "./components/App"
-import {BrowserRouter} from "react-router-dom" 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"))
+import { HashRouter } from "react-router-dom" 
+ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById("root"))
